c66579639afd2a8ba93785d1840ddf4ed2d61387
izng-code <129785832+izng-code@users.noreply.github.com>
2023-07-11 12:31:29 +0200

    Condense code and stop once one player reaches 5 points

--- janken.js
+++ janken.js
@@ -34,117 +34,43 @@ function playRound(playerSelection, computerSelection) {
 let playerScore = 0;
 let computerScore = 0;
 let ties = 0;
-function game() {
-    // initializes selections
-    const playerSelection = getPlayerSelection();
-    const computerSelection = getComputerChoice();
-    // plays a round and logs the result
-    let round = playRound(playerSelection,computerSelection);
-    console.log(round)
-    // updates score by string matching
-    if (round.includes("You have won!")) {
-        playerScore++;
-    }
-    else if (round.includes("It's a tie!")) {
-        ties++
-    }
-    else {
-        computerScore++;
-    }
-}
-
 let playerScoreHTML = document.getElementById("score-user");
 let computerScoreHTML = document.getElementById("score-com");
 let resultHTML = document.getElementById("result");
 
-document.getElementById("rock").addEventListener("click", function() {
-    let round = playRound("rock", getComputerChoice());
-    // updates score by string matching
-    if (round.includes("You have won!")) {
-        playerScore++;
-    }
-    else if (round.includes("It's a tie!")) {
-        ties++
-    }
-    else {
-        computerScore++;
-    }   
-    playerScoreHTML.innerText = playerScore;
-    computerScoreHTML.innerText = computerScore;
-    resultHTML.innerText = round;
-    if (playerScoreHTML.innerText === "5") {
-        let div = document.createElement("div");
-        div.innerText = "The game is over! You have won!";
-        document.querySelector("body").appendChild(div)
-    }
-    else if (computerScoreHTML.innerText === "5") {
-        let div = document.createElement("div");
-        div.innerText = "The game is over! The computer has won!"
-        document.querySelector("body").appendChild(div)
-    }      
-});
-document.getElementById("paper").addEventListener("click", function() {
-    let round = playRound("paper", getComputerChoice());
-    // updates score by string matching
-    if (round.includes("You have won!")) {
-        playerScore++;
-    }
-    else if (round.includes("It's a tie!")) {
-        ties++
-    }
-    else {
-        computerScore++;
-    }
-    playerScoreHTML.innerText = playerScore;
-    computerScoreHTML.innerText = computerScore;
-    resultHTML.innerText = round;
-    if (playerScoreHTML.innerText === "5") {
-        let div = document.createElement("div");
-        div.innerText = "The game is over! You have won!";
-        document.querySelector("body").appendChild(div)
-    }
-    else if (computerScoreHTML.innerText === "5") {
-        let div = document.createElement("div");
-        div.innerText = "The game is over! The computer has won!"
-        document.querySelector("body").appendChild(div)
-    }      
-});
-document.getElementById("scissors").addEventListener("click", function() {
-    let round = playRound("scissors", getComputerChoice());
-    // updates score by string matching
-    if (round.includes("You have won!")) {
-        playerScore++;
-    }
-    else if (round.includes("It's a tie!")) {
-        ties++
-    }
-    else {
-        computerScore++;
-    }
-    playerScoreHTML.innerText = playerScore;
-    computerScoreHTML.innerText = computerScore;
-    resultHTML.innerText = round;
-    if (playerScoreHTML.innerText === "5") {
-        let div = document.createElement("div");
-        div.innerText = "The game is over! You have won!";
-        document.querySelector("body").appendChild(div)
-    }
-    else if (computerScoreHTML.innerText === "5") {
-        let div = document.createElement("div");
-        div.innerText = "The game is over! The computer has won!"
-        document.querySelector("body").appendChild(div)
-    }      
-});
-
-console.log(playerScore)
-console.log(computerScore)
-console.log(ties)
-if (playerScore > computerScore) {
-    console.log("You have won!");
-}
-else if (playerScore === computerScore) {
-    console.log("It's a tie!");
-}
-else {
-    console.log("You have lost!");
-}
+function playGameTo5() {    
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            if (playerScore < 5 && computerScore < 5) {
+                let round = playRound(button.id, getComputerChoice());
+                // updates score by string matching
+                if (round.includes("You have won!")) {
+                    playerScore++;
+                }
+                else if (round.includes("It's a tie!")) {
+                    ties++
+                }
+                else {
+                    computerScore++;
+                }   
+                playerScoreHTML.innerText = playerScore;
+                computerScoreHTML.innerText = computerScore;
+                resultHTML.innerText = round;
+                if (playerScoreHTML.innerText === "5") {
+                    let div = document.createElement("div");
+                    div.innerText = "The game is over! You have won!";
+                    document.querySelector("body").appendChild(div);
+                    return;
+                }
+                else if (computerScoreHTML.innerText === "5") {
+                    let div = document.createElement("div");
+                    div.innerText = "The game is over! The computer has won!"
+                    document.querySelector("body").appendChild(div);
+                    return;
+                }
+            };
+        });
+    });      
+};
+playGameTo5();
